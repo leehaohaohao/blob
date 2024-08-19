6017541d4f0e4afd90df160742868ec7a365be2b
lihao <805459342@qq.com>
2024-08-19 20:48:15 +0800

    管理员问题反馈模块功能开发完毕

---
+++ src/main/java/com/lihao/entity/dto/FeedBackDto.java
@@ -0,0 +1,19 @@
+package com.lihao.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+public class FeedBackDto {
+    private String feedbackId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date time;
+    private String content;
+    private String file;
+    private String type;
+    private String name;
+}
